8c8d4ff5bfb6b9a8089a5363b8678838025dcdf3
lrpe <lassep@protonmail.com>
2021-12-02 11:56:16 +0100

    Add project files.

---
+++ AdventOfCode.2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode._2021;
+
+// Day 1: Sonar Sweep
+var sonarSweep = new SonarSweep();
+Console.WriteLine("Day 1: Sonar Sweep");
+
+// Part 1
+int result = sonarSweep.CountMeasurementsLargerThanPrevious();
+Console.WriteLine($"There are {result} measurements that are larger than the previous.");
+
+// Part 2
+List<int> batchMeasurements = sonarSweep.BatchMeasurements();
+int batchResult = sonarSweep.CountMeasurementsLargerThanPrevious(batchMeasurements);
+Console.WriteLine($"There are {batchResult} batch measurements that are larger than the previous.");
---
+++ AdventOfCode.2021/SonarSweep.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AdventOfCode._2021;
+
+public class SonarSweep
+{
+    public SonarSweep()
+    {
+        Measurements = File.ReadLines("SonarSweep.txt").Select(int.Parse);
+    }
+
+    public IEnumerable<int> Measurements { get; private set; }
+
+    public int CountMeasurementsLargerThanPrevious([Optional] IEnumerable<int> measurements)
+    {
+        measurements ??= Measurements;
+
+        int measurementsLargerThanPrevious = 0;
+
+        // Start at index 1, since first item has no previous measurement.
+        for (int index = 1; index < measurements.Count(); index++)
+        {
+            if (measurements.ElementAt(index) > measurements.ElementAt(index - 1))
+            {
+                measurementsLargerThanPrevious++;
+            }
+        }
+
+        return measurementsLargerThanPrevious;
+    }
+
+    public List<int> BatchMeasurements([Optional] IEnumerable<int> measurements, int batchSize = 3)
+    {
+        measurements ??= Measurements;
+
+        IEnumerable<int> batchMeasurements = measurements
+            .SkipLast(batchSize - 1)
+            .Select((measurement, index) => measurement + measurements.Skip(index + 1).Take(batchSize - 1).Sum());
+
+        return batchMeasurements.ToList();
+    }
+}
